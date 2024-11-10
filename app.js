e4f2b7579eafec85706bd28a525af8b1870d99d2
Muhammad Ali <alikhancss27@gmail.com>
2024-11-10 19:42:43 +0500

    ali

---
+++ app.js
@@ -0,0 +1,19 @@
+// var name1 = document.getElementById("username-1")
+// name1=name1.value
+// var name2 = document.getElementById("username-2")
+// name2=name2.value
+
+function coinhead(){
+  var coinvalue="head"
+  document.getElementById("user-message").innerHTML=`<span style="color:black;">You have chosen ${coinvalue}</span>`
+}
+function cointail(){
+  var coinvalue="tail"
+  document.getElementById("user-message").innerHTML=`<span style="color:black;">You have chosen ${coinvalue}</span>`
+
+}
+
+// function cointoss(){
+//   var value=
+// }
+
