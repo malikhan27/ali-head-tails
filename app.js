65e4b6adc67e63912143880ee9e849f0e9c8411e
Muhammad Ali <alikhancss27@gmail.com>
2024-11-11 00:00:34 +0500

    game

--- app.js
+++ app.js
@@ -1,19 +1,63 @@
-// var name1 = document.getElementById("username-1")
-// name1=name1.value
-// var name2 = document.getElementById("username-2")
-// name2=name2.value
+var name1 = document.getElementById("username-1")
+var name2 = document.getElementById("username-2")
+var coinvalue 
 
-function coinhead(){
-  var coinvalue="head"
-  document.getElementById("user-message").innerHTML=`<span style="color:black;">You have chosen ${coinvalue}</span>`
+function getvalues(){
+  name1 = name1.value
+  name2 = name2.value
+}
+
+
+
+function coinhead(){ 
+  coinvalue="head"
+  document.getElementById("user-message").innerHTML=`<span style="color:black;">You have chosen ${coinvalue} ${name1}</span>`
 }
 function cointail(){
-  var coinvalue="tail"
-  document.getElementById("user-message").innerHTML=`<span style="color:black;">You have chosen ${coinvalue}</span>`
+var name1 = document.getElementById("username-1")
+var name2 = document.getElementById("username-2")
+name1 = name1.value
+name2 = name2.value
+
+   coinvalue="tail"
+  document.getElementById("user-message").innerHTML=`<span style="color:black;">You have chosen ${coinvalue} ${name1}</span>`
 
 }
 
-// function cointoss(){
-//   var value=
-// }
+function stillimage(){
+
+  document.getElementById("image").src="assets/still-pic.png"
+  
+}
+
+function toss(){
+  document.getElementById("image").src="assets/giphy.gif"
+}
+   
+
+function tossthecoin(){ 
+  var coinarr=["head","tail"]
+  var randomvalue= Math.floor(Math.random()*2)
+  if(coinarr[randomvalue]===coinvalue){
+    Swal.fire({
+      title: "Congratulations! "+name1 , 
+      text: "its "+coinvalue+" You Won" ,
+      icon: "success"
+    });}
+    else{
+      Swal.fire({
+        title: "Congratulations! " +name2 , 
+      text: "its "+coinarr[randomvalue]+" You Won" ,
+      icon: "success"
+      });
+    }
+setTimeout(stillimage,2000)
+    
+}
+
+
+
+
+
+
 
