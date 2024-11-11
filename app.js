daeac807f50fdb3d9d1f964392e7e1c6f1b3f851
Muhammad Ali <alikhancss27@gmail.com>
2024-11-11 05:30:51 +0500

    new adjustments

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-var name1 = document.getElementById("username-1")
-var name2 = document.getElementById("username-2")
-var coinvalue 
+var name1 = "admin"
+var name2 = "admin"
+var coinvalue ="xyz"
 
 function getvalues(){
-  name1 = name1.value
-  name2 = name2.value
+  name1 = document.getElementById("username-1")
+  name2 = document.getElementById("username-2")
+  name1=name1.value
+  name2=name2.value
 }
 
+
+function divrevive(){
+  if(name1!=="" && name2!=="" && name1!==undefined){
+    
+    document.getElementById("first").classList.remove("d-none")
+    document.getElementById("second").classList.add("d-none")
+    
+ }
+  else{
+    Swal.fire({
+      title: "Sorry! USER"  , 
+    text: "please fill and submit the fileds to participate" ,
+    icon: "error"
+    })
+    
+
+}
+}
+
+   
 
 
 function coinhead(){ 
@@ -17,6 +39,13 @@ function cointail(){
    coinvalue="tail"
   document.getElementById("user-message").innerHTML=`<span style="color:black;">You have chosen ${coinvalue} ${name1}</span>`
 
+}
+
+function buttonrevive(){
+  if(coinvalue!=="xyz"||coinvalue!=="xyz"){
+    document.getElementById("third").classList.remove("d-none")
+  }
+  
 }
 
 function stillimage(){
@@ -50,6 +79,7 @@ function tossthecoin(){
 setTimeout(stillimage,2000)
     
 }
+
 
 
 
